ddc4aba66cee220fa9645d152bdd43acd4e93afe
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-09-06 22:53:10 +0700

    add: hero layout component

---
+++ src/components/layouts/HeroLayout.jsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+import AnimatedText from "../ui/AnimatedText";
+
+export default function HeroLayout() {
+  return (
+    <div className="hero-wrapper z-50">
+      <div className="headline flex flex-col gap-5 w-full md:w-[700px]">
+        <h1 className="text-[54px] leading-tight md:text-7xl text-center text-transparent font-figtree font-extrabold bg-clip-text bg-gradient-to-br from-primary-dark to-primary">
+          A Passionate Website Developer
+        </h1>
+        <AnimatedText
+          el="span"
+          text="👋 Hi, I’m Faishal Hilmy. I bring two years of hands-on experience as a skilled Website Developer"
+          className="text-primary-dark md:text-xl text-center font-medium"
+        />
+      </div>
+      <div className="call-to-action flex justify-center gap-2.5 mt-7">
+        <Link to="/projects">
+          <div className="relative rounded-full font-semibold text-white bg-gradient-to-r from-white to-accent-success p-[2px] w-fit">
+            <span className="block rounded-full px-6 py-3 text-sm bg-[linear-gradient(90deg,#AD95EB,#7362E2,#5A66E1,#7FC5C5)]">
+              📂 View Projects
+            </span>
+          </div>
+        </Link>
+        <a
+          href="https://drive.google.com/file/d/19b3FRFy5hI9KZ-xD4iflx8z56D8seqEr/view?usp=sharing"
+          target="_blank"
+        >
+          <div className="relative rounded-full font-semibold text-white bg-gradient-to-r from-white to-accent-success p-[2px] w-fit">
+            <span className="block rounded-full px-6 py-3 text-sm bg-[linear-gradient(90deg,#AD95EB,#7362E2,#5A66E1,#7FC5C5)]">
+              📄 View My CV
+            </span>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
+}
