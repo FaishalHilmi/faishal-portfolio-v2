4d58b60d4857763210d1151c7c5f011a11524a18
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-09-08 23:31:07 +0700

    feat: add react router and scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,17 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Projects from "./pages/projects";
+import ScrollToTop from "./utils/scrollToTop";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank"></a>
-        <a href="https://react.dev" target="_blank"></a>
-      </div>
-      <h1 className="font-satoshi text-primary">Vite + React</h1>
-      <div className="card">
-        <button
-          className="bg-blue-200 font-mono p-2 rounded font-sa"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </Router>
   );
 }
 
