52ff6596fb9382be6f883314693b308176df0198
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-09-09 21:52:02 +0700

    chore: add custom style

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ export default {
       colors: {
         base: "#F3F4F4",
         primary: {
-          DEFAULT: "#2F2289", // warna utama
+          DEFAULT: "#2F2289", 
           light: "#7362E2",
           dark: "#272343",
         },
@@ -23,6 +23,18 @@ export default {
           success: "#ADFDA2",
           info: "#83B7FF",
         },
+      },
+      backgroundImage: {
+        "gradient-pink": "url('/images/gradient-pink.png')",
+        "gradient-purple": "url('/images/gradient-purple.png')",
+        "gradient-yellow": "url('/images/gradient-yellow.png')",
+      },
+      boxShadow: {
+        light: "0 4px 6px rgba(0, 0, 0, 0.15)",
+        strong: "0 4px 12px rgba(0, 0, 0, 0.15)",
+      },
+      animation: {
+        "spin-slow": "spin 6s linear infinite",
       },
     },
   },
