07479af947c21dc2d6cedd8747e2d19095d170a7
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-09-09 21:51:26 +0700

    add: experiences data

---
+++ data/experiences.js
@@ -0,0 +1,23 @@
+export const experiences = [
+  {
+    id: 1,
+    role: "Cloud Computing Cohort",
+    company: "Bangkit Academy",
+    location: "Indonesia",
+    date: "Feb - Jul 2024",
+  },
+  {
+    id: 2,
+    role: "Frontend Developer (Freelance)",
+    company: "Undankin",
+    location: "Medan, Indonesia",
+    date: "Dec 2022 - Jan 2024",
+  },
+  {
+    id: 3,
+    role: "Web Developer (Freelance)",
+    company: "PT. Mahkota Empat Pilar",
+    location: "Medan, Indonesia",
+    date: "Jun - Jul 2023",
+  },
+];
