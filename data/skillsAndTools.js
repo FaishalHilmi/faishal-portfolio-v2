a33eceedef42df8bc9a8c3d7132420daf56d3d88
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-09-09 21:51:13 +0700

    add: skills and tools data

---
+++ data/skillsAndTools.js
@@ -0,0 +1,38 @@
+export const skillsAndTools = [
+  {
+    id: 1,
+    title: "Nextjs",
+  },
+  {
+    id: 2,
+    title: "Reactjs",
+  },
+  {
+    id: 3,
+    title: "Expressjs",
+  },
+  {
+    id: 4,
+    title: "Laravel",
+  },
+  {
+    id: 5,
+    title: "Flask",
+  },
+  {
+    id: 6,
+    title: "Postgresql",
+  },
+  {
+    id: 7,
+    title: "Mongodb",
+  },
+  {
+    id: 8,
+    title: "Github",
+  },
+  {
+    id: 9,
+    title: "Postman",
+  },
+];
