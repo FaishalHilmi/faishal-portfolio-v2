a5234748037da3f3a1a883347066e45f2588d8b5
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-09-10 21:04:32 +0700

    chore: change headline to be h2

--- src/components/layouts/SkillsAndToolsLayout.jsx
+++ src/components/layouts/SkillsAndToolsLayout.jsx
@@ -5,9 +5,9 @@ export default function SkillsAndToolsLayout() {
   return (
     <div className="skills-tools-wrapper">
       <div className="px-5 md:px-16 lg:px-36">
-        <h1 className="uppercase text-center text-lg md:text-xl font-bold mb-5">
+        <h2 className="uppercase text-center text-lg md:text-xl font-bold mb-5">
           Skills & Tools
-        </h1>
+        </h2>
         <AnimatedText
           el="h3"
           text="Here are the technologies and tools I enjoy working with"
@@ -19,12 +19,10 @@ export default function SkillsAndToolsLayout() {
       <div className="py-5 md:py-7 border bg-gradient-custom overflow-hidden">
         <ul className="uppercase text-3xl text-white flex items-center gap-5 font-bold whitespace-nowrap animate-marquee">
           {[...skillsAndTools, ...skillsAndTools].map((skill, index) => (
-            <div key={index} className="flex items-center gap-5">
-              <li>{skill.title}</li>
-              <li>
-                <div className="w-2 h-2 bg-white rounded-full"></div>
-              </li>
-            </div>
+            <li key={index} className="flex items-center gap-5">
+              <span>{skill.title}</span>
+              <span className="w-2 h-2 bg-white rounded-full"></span>
+            </li>
           ))}
         </ul>
       </div>
