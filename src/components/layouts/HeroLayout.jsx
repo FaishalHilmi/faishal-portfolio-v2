2284647a65fb57399b6a4968d90d245e837661a7
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-12-16 23:37:59 +0700

    chore: change the heading to fullname

--- src/components/layouts/HeroLayout.jsx
+++ src/components/layouts/HeroLayout.jsx
@@ -5,12 +5,12 @@ export default function HeroLayout() {
   return (
     <div className="hero-wrapper z-50">
       <div className="headline flex flex-col gap-5 w-full md:w-[700px]">
-        <h1 className="text-[54px] leading-tight md:text-7xl text-center text-transparent font-figtree font-extrabold bg-clip-text bg-gradient-to-br from-primary-dark to-primary">
-          A Passionate Website Developer
+        <h1 className="text-[54px] leading-tight md:text-7xl pb-1 text-center text-transparent font-figtree font-extrabold bg-clip-text bg-gradient-to-br from-primary-dark to-primary">
+          Faishal Hilmy Fadhilah Ginting
         </h1>
         <AnimatedText
-          el="span"
-          text="👋 Hi, I’m Faishal Hilmy. I bring two years of hands-on experience as a skilled Website Developer"
+          el="p"
+          text="👋 Hi, I’m A Passionate Website Developer with two years of hands-on experience"
           className="text-primary-dark md:text-xl text-center font-medium"
         />
       </div>
@@ -23,7 +23,7 @@ export default function HeroLayout() {
           </div>
         </Link>
         <a
-          href="https://drive.google.com/file/d/159gX3EfJYDDajvdwK18C6tSsMLrezKRQ/view?usp=sharing"
+          href="https://drive.google.com/file/d/1EgHuo7qji0SOPAFAaSbiQHyZeAfM8Uus/view?usp=sharing"
           target="_blank"
         >
           <div className="relative rounded-full font-semibold text-white bg-gradient-to-r from-white to-accent-success p-[2px] w-fit">
